a9cdc7a0c54194e5a41ea7bcb97e2fb5bfdc3d96
Kaylum303 <kaylum303@gmail.com>
2025-10-18 23:26:14 +0200

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ async function loadPatients(){
     list.innerHTML = '';
     patients.forEach(p => {
     const li = document.createElement('li');
-    li.innerHTML = `${p.name} ${p.surename} Wiek:${p.age}, Ciśnienie:${p.pressure}, Temperatura:${p.temperature}    <button onclick="deletePatient(${p.id})"> Delete </button>   <button onclick="editPatient(${p.id})"> Edit </button>`;
+    li.innerHTML = `${p.name} ${p.surename} Wiek:${p.age}, Ciśnienie:${p.pressure}, Temperatura:${p.temperature}, Tętno:${p.pulse}, PESEL:$(p.pesel) <button onclick="deletePatient(${p.id})"> Delete </button>   <button onclick="editPatient(${p.id})"> Edit </button>`;
     list.appendChild(li);
     });
 }
@@ -24,6 +24,8 @@ async function editPatient(id){
     document.getElementById('age').value = patient.age;
     document.getElementById('pressure').value = patient.pressure;
     document.getElementById('temperature').value = patient.temperature;
+	document.getElementById('pulse').value = patient.pulse;     //nowe pole tętno
+	document.getElementById('pesel').value = patient.pesel;     //noew pole pesel
 
     editId = id;
 
@@ -34,7 +36,10 @@ async function editPatient(id){
         surename: document.getElementById('surename').value,
         age: parseInt(document.getElementById('age').value),
         pressure: parseInt(document.getElementById('pressure').value),
-        temperature: parseFloat(document.getElementById('temperature').value)
+        temperature: parseFloat(document.getElementById('temperature').value),
+		pulse: parseInt(document.getElementById('pulse').value), //nowe pole tętno
+		pesel: document.getElementById('pesel').value)          //noew pole pesel
+		
     };
     if(editId){
         await fetch(`${API_URL}/${editId}`,{
