4061e7d7ac1f148ff4c09c7c8a98ef64ae5c1fce
xChoina <barti5416@gmail.com>
2025-10-27 14:13:00 +0100

    Update models.py
    dodanie tabeli users i hashowanie hasła

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Float, Column, Integer, String
+from sqlalchemy import Float, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = declarative_base()
 
@@ -7,7 +10,7 @@ class Patient(Base):
     __tablename__ = 'patients'
     id = Column(Integer, primary_key=True)
     name = Column(String(100),nullable=False)
-    surename = Column(String(100),nullable=False)
+    surname = Column(String(100),nullable=False)
     age = Column(Integer)
     pressure = Column(Integer)
     temperature = Column(Float)
@@ -15,4 +18,19 @@ class Patient(Base):
     pesel = Column(String(11), nullable=False)  #deklaracja kolumny pesel
 
     def __repr__(self):
-        return f"<Patient(id={self.id}, name='{self.name} {self.surename}' , pressure={self.pressure} , temperature{self.temperature} , pulse{self.pulse}, pesel{self.pesel} >" #dodanie dwóch nowych pól
+        return f"<Patient(id={self.id}, name='{self.name} {self.surname}' , pressure={self.pressure} , temperature={self.temperature} , pulse={self.pulse}, pesel={self.pesel} >" #dodanie dwóch nowych pól
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(100),nullable=False, unique=True)
+    password_hash = Column(String(200), nullable=False)
+    role = Column(String(100), nullable=False, default='USER')
+    created_date = Column(DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+            self.password_hash=generate_password_hash(password)
+
+    def check_password(self, password):
+            return check_password_hash(self.password_hash, password)
+
